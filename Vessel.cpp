123d9fcd5b71258dc6020f1c5ec8f995ea721c0e
FangBoHan <40261483+FangBoHan@users.noreply.github.com>
2018-06-21 10:15:09 +0800

    Update Vessel.cpp

--- Vessel.cpp
+++ Vessel.cpp
@@ -1,5 +1,6 @@
 #include "Vessel.h"
 #include "MyForm.h"
+#include <math.h>
 
 
 #pragma region get()
@@ -91,6 +92,26 @@ double Shell::getY()
 {
 	return this->y;
 }
+
+double Shell::getATK()
+{
+	return this->attack;
+}
+
+double Shell::getDesX()
+{
+	return this->destination_X;
+}
+
+double Shell::getDesY()
+{
+	return this->destination_Y;
+}
+
+int Vessel::getAngle()
+{
+	return this->angle;
+}
 #pragma endregion
 #pragma region set()
 void Vessel::setHp(double hp)
@@ -148,56 +169,6 @@ void Vessel::setY(double y)
 	this->y = y;
 }
 
-void Vessel::moving()
-{
-	double newX, newY;
-
-	double xBound = x;
-	double yBound = y;
-
-	newX = currentSpeed * cos(angle / 180 * PI);
-	newY = currentSpeed * sin(angle / 180 * PI);
-
-	if (x + newX <= 0.0)
-	{
-		xBound = 0.0;
-		newY *= ((x - 0.0) / newX);
-		currentSpeed = 0.0;
-	}
-	else if (x + newX >= 20.0)
-	{
-		xBound = 20.0;
-		newY *= ((20.0 - x) / newX);
-		currentSpeed = 0.0;
-	}
-
-	if (y + newY <= 0.0)
-	{
-		yBound = 0.0;
-		newX *= ((y - 0.0) / newY);
-		currentSpeed = 0.0;
-	}
-
-	else if (y + newY >= 20.0)
-	{
-		yBound = 20.0;
-		newX *= ((20.0 - y) / newY);
-		currentSpeed = 0;
-	}
-
-	if (x != xBound)
-		x = xBound;
-	else
-		x += newX;
-
-	if (y != yBound)
-		y = yBound;
-	else
-		y += newY;
-	
-
-}
-
 void Vessel::setAngle(int angle)
 {
 	this->angle = angle;
@@ -214,56 +185,18 @@ void Shell::setY(double y)
 	this->y = y;
 }
 
-double Shell::getATK()
-{
-	return this->attack;
-}
-
-double Shell::getDesX()
-{
-	return this->destination_X;
-}
-
-double Shell::getDesY()
-{
-	return this->destination_X;
-}
-
-void  Shell::moving()
-{
-	double xRange, yRange;
-	xRange = destination_X - x;
-	yRange = destination_Y - y;
-
-	double range = pow(xRange, 2) + pow(yRange, 2);
-	range = sqrt(range);
-
-	x += speed * (xRange / range);
-	y += speed * (yRange / range);
-	   //v目的地在原位置左邊，且移動後超過目的地    v目的地在原位置右邊
-	if ((xRange < 0 && destination_X > x) || (xRange > 0 && destination_X < x))
-	{
-		x = destination_X;
-	}
-	   //v目的地在原位置上面                     v在原位置下面
-	if ((yRange < 0 && destination_Y > y) || (yRange > 0 && destination_Y < y))
-	{
-		y = destination_Y;
-	}
-}
-
 #pragma endregion
 #pragma region 建構子
 CV::CV()
 {
 	hp = 5;
-	maxSpeed = 1;
+	maxSpeed = 1 / 60.0;
 	currentSpeed = 0;
 	atkRange = 25;
 	atkCD = 0;
 	defRange = 5;
 	defCD = 0;
-	weaponSpeed = (4 / 60);
+	weaponSpeed = (4 / 60.0);
 	weaponAtt = 3;
 	type = "CV";
 	angle = 0;
@@ -272,13 +205,13 @@ CV::CV()
 BB::BB()
 {
 	hp = 4;
-	maxSpeed = 1;
+	maxSpeed = 1 / 60.0;
 	currentSpeed = 0;
 	atkRange = 20;
 	atkCD = 0;
 	defRange = 10;
 	defCD = 0;
-	weaponSpeed = (2 / 60);
+	weaponSpeed = (2 / 60.0);
 	weaponAtt = 3;
 	type = "BB";
 	angle = 0;
@@ -287,13 +220,13 @@ BB::BB()
 CG::CG()
 {
 	hp = 3;
-	maxSpeed = 2;
+	maxSpeed = 2 / 60.0;
 	currentSpeed = 0;
 	atkRange = 15;
 	atkCD = 0;
 	defRange = 15;
 	defCD = 0;
-	weaponSpeed = (3 / 60);
+	weaponSpeed = (3 / 60.0);
 	weaponAtt = 2;
 	type = "CG";
 	angle = 0;
@@ -302,13 +235,13 @@ CG::CG()
 DD::DD()
 {
 	hp = 2;
-	maxSpeed = 3;
+	maxSpeed = 3 / 60.0;
 	currentSpeed = 0;
 	atkRange = 10;
 	atkCD = 0;
 	defRange = 20;
 	defCD = 0;
-	weaponSpeed = (3 / 60);
+	weaponSpeed = (3 / 60.0);
 	weaponAtt = 1;
 	type = "DD";
 	angle = 0;
@@ -323,5 +256,70 @@ Shell::Shell(string name, double x, double y, double destination_X, double desti
 	this->destination_Y = destination_Y;
 	this->speed = speed;
 	this->attack = attack;
+}
+#pragma endregion
+#pragma region 其他
+void  Shell::moving()
+{
+	double xRange, yRange;
+	xRange = destination_X - x;
+	yRange = destination_Y - y;
+
+	double range = pow(xRange, 2) + pow(yRange, 2);
+	range = sqrt(range);
+
+	x += speed * (xRange / range);
+	y += speed * (yRange / range);
+	//v目的地在原位置左邊，且移動後超過目的地    v目的地在原位置右邊
+	if ((xRange < 0 && destination_X > x) || (xRange > 0 && destination_X < x))
+	{
+		x = destination_X;
+	}
+	//v目的地在原位置上面                     v在原位置下面
+	if ((yRange < 0 && destination_Y > y) || (yRange > 0 && destination_Y < y))
+	{
+		y = destination_Y;
+	}
+}
+
+void Vessel::moving()
+{
+	double newX, newY;	// x的變化量, y的變化量
+
+	double xBound = x;
+	double yBound = y;
+
+	newX = currentSpeed * cos(angle / 180 * PI);
+	newY = currentSpeed * sin(angle / 180 * PI);
+
+	if (x + newX <= 0.0)
+	{
+		x = 0.0;
+		currentSpeed = 0.0;
+	}
+	else if (x + newX >= 20.0)
+	{
+		x = 20.0;
+		currentSpeed = 0.0;
+	}
+	else
+	{
+		x = x + newX;
+	}
+
+	if (y + newY <= 0.0)
+	{
+		yBound = 0.0;
+		currentSpeed = 0.0;
+	}
+	else if (y + newY >= 20.0)
+	{
+		yBound = 20.0;
+		currentSpeed = 0;
+	}
+	else
+	{
+		y = y + newY;
+	}
 }
 #pragma endregion
